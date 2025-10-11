f580b150745c5c4de003df207b44b2c3edf71b9f
chalananimsar <chalananimsara50@gmail.com>
2025-10-10 23:33:56 -0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/chalananimsar/suddaa-mt/blob/main/images/WhatsApp%20Image%202025-09-06%20at%205.32.14%20AM.jpeg?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 sudda-mt Is Alive Now😍*",
 BOT_OWNER: '94779890822',  // Replace with the owner's phone number
 
